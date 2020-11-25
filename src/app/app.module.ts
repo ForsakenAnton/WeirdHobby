d4f0a2c12a1e8df06d35db605f42004e4c624595
ForsakenAnton <forsakenanton@gmail.com>
2020-11-25 19:38:15 +0200

    weird hobby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HobbyComponent } from './hobby/hobby.component';
+import { UserComponent } from './user/user.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { MyKnowledgeComponent } from './my-knowledge/my-knowledge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HobbyComponent,
+    UserComponent,
+    MyWorkComponent,
+    MyKnowledgeComponent
   ],
   imports: [
     BrowserModule
